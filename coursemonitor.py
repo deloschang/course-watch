65c5a0e9d9d4358f0ceaee8a35c16451f27177c4
Delos Chang <delosfchang@gmail.com>
2013-03-25 11:51:14 -0400

    takes in email and continuously checks until opening

--- coursemonitor.py
+++ coursemonitor.py
@@ -1,9 +1,53 @@
+#!/usr/bin/python
 import urllib
 import urllib
 import urllib2
 import cookielib
 from html5lib import HTMLParser, treebuilders
 
+# Adapted from http://kutuma.blogspot.com/2007/08/sending-emails-via-gmail-with-python.html
+ 
+import getpass
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+ 
+gmail_user = "you@should.login"
+gmail_pwd = "wrong password"
+ 
+def login(user):
+   global gmail_user, gmail_pwd
+   gmail_user = user
+   gmail_pwd = getpass.getpass('Password for %s: ' % gmail_user)
+ 
+def mail(to, subject, text, attach=None):
+   msg = MIMEMultipart()
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+   msg.attach(MIMEText(text))
+   if attach:
+      part = MIMEBase('application', 'octet-stream')
+      part.set_payload(open(attach, 'rb').read())
+      Encoders.encode_base64(part)
+      part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attach))
+      msg.attach(part)
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   mailServer.close()
+ 
+# Example!
+def example(available):
+   email = raw_input("Your Gmail address: ")
+   login(email)
+   mail(email, "Course opening Available", str(available)+" spots available")
 
 
 def get_spaces_available(dept_abbr, course_num):
@@ -37,17 +81,17 @@ def get_spaces_available(dept_abbr, course_num):
     return available
 
 
-# elementary oceanography
+# test with elementary oceanography
 available = get_spaces_available('EARS', 003)
+
+# test with email
+example(available)  
 
 while available == 0:
     # check again for space
     available = get_spaces_available('EARS', 003)
 else:
     # respond and email me if course update
+    example(available)
 
-    
-
-
-
-
+ 
